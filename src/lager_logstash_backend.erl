1dfa5d150b9c816de185a32bdcb98ea91fad0579
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-12-17 16:40:09 +0100

    Handle responses from the redis cluster correctly.

--- src/lager_logstash_backend.erl
+++ src/lager_logstash_backend.erl
@@ -187,7 +187,7 @@ connect({udp, Host, Port}) ->
     end.
 
 send({redis, Client, _Mref, Key}, Event) ->
-    ok = eredis:q(Client, ["RPUSH", Key, Event]),
+    {ok, _Count} = eredis:q(Client, ["RPUSH", Key, Event]),
     ok;
 send({udp, Sock, Host, Port}, Event) ->
     gen_udp:send(Sock, Host, Port, Event);
